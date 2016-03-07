b39c8b2cf6a7a3c6b55a5f1c4121648d91edd09b
mbmahdiiii <mbmahdi650@gmail.com>
2016-03-07 20:58:25 +0330

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -14,14 +14,12 @@
 do
 
 function run(msg, matches)
-  return 'DBTeam Bot V1 Supergroups\nAn advanced Administration bot based on yagop/telegram-bot \n\nhttp://github.com/Josepdal/DBTeam\n\nDevelopers :\n@MaSkAoS\n@Josepdal\n\nSupport :\n@skneos\n@iicc1\n@Serx666' 
-  --[[ Checkout https://github.com/Josepdal/DBTeam
-  --GNU GPL v2 license.]]
+  return 
 end
 
 return {
   patterns = {
-    "^#version$"
+    "^!version$"
   }, 
   run = run 
 }
